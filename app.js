c9d5a4461d8826855459fb7f879f2b29c73329f9
scouser <181530843+Demiladepy@users.noreply.github.com>
2025-10-21 16:16:49 -0700

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use('/strings', stringRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Server is running");
+  res.send("Server is working");
 });
 app.use((req, res) => {
   res.status(404).json({ error: 'Route not found' });
